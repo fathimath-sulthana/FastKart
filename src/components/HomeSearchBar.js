b98493f7c4d92fd0e02498ec551128003f6031e7
sulthana <sulthana.inmakes@gmail.com>
2022-12-31 09:43:13 +0530

    sulthana

---
+++ src/components/HomeSearchBar.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Searchbar } from 'react-native-paper'
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
+const HomeSearchBar = () => {
+  return (
+    <View style={styles.container}>
+    <Searchbar
+    placeholder='search here...'
+    style={styles.searchBar}
+    theme={themes}
+    iconColor='#0caf9a'
+    
+    />
+   
+    </View>
+  )
+}
+
+export default HomeSearchBar
+const themes ={
+  color:'#0caf9a'
+}
+const styles = StyleSheet.create({
+    container:{
+      padding:15,
+     
+    },
+    searchBar:{
+      backgroundColor:'#fafafa'
+    },
+    iconStyle:{
+    alignSelf:'flex-end',
+   
+    }
+})
