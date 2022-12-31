be141eb46321d2d32315e8c9a8a5bff1ea8df878
sulthana <sulthana.inmakes@gmail.com>
2022-12-31 09:52:03 +0530

    sulthana

---
+++ src/components/shopContainer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const shopContainer = () => {
+  return (
+    <View>
+      <Text>shopContainer</Text>
+    </View>
+  )
+}
+
+export default shopContainer
+
+const styles = StyleSheet.create({})
